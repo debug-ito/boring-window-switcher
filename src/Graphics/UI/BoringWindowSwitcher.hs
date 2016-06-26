dc1ff31ff93129c779f24f0c0dd274441abaf6f7
Toshio Ito <debug.ito@gmail.com>
2016-06-26 17:19:17 +0900

    Dialog: create window and show it.

--- src/Graphics/UI/BoringWindowSwitcher.hs
+++ src/Graphics/UI/BoringWindowSwitcher.hs
@@ -6,5 +6,23 @@
 module Graphics.UI.BoringWindowSwitcher
        (main) where
 
+import Control.Monad (void)
+import Control.Monad.IO.Class (liftIO)
+import Graphics.UI.BoringWindowSwitcher.Internal.Control
+  ( withControl, selectableWindows
+  )
+import Graphics.UI.BoringWindowSwitcher.Internal.Dialog (createDialog)
+
+import qualified Graphics.UI.Gtk as Gtk
+
 main :: IO ()
-main = undefined
+main = do
+  void $ Gtk.initGUI
+  withControl $ \control -> do
+    wins <- selectableWindows control
+    dialog <- createDialog wins
+    void $ Gtk.on dialog Gtk.deleteEvent $ do
+      liftIO $ Gtk.mainQuit
+      return False
+    Gtk.widgetShowAll dialog
+    Gtk.mainGUI
--- src/Graphics/UI/BoringWindowSwitcher/Internal/Dialog.hs
+++ src/Graphics/UI/BoringWindowSwitcher/Internal/Dialog.hs
@@ -8,10 +8,19 @@ module Graphics.UI.BoringWindowSwitcher.Internal.Dialog
        ( createDialog
        ) where
 
+import qualified Graphics.UI.Gtk as Gtk
+import Graphics.UI.Gtk (AttrOp((:=)))
+
 import Graphics.UI.BoringWindowSwitcher.Internal.Control (Window)
 
-data Dialog
+createDialog :: [Window] -> IO Gtk.Window
+createDialog wins = do
+  dialog_window <- Gtk.windowNew
+  Gtk.set dialog_window [Gtk.windowTitle := "Boring Window Switcher"]
+  wlist <- createWindowList wins
+  Gtk.containerAdd dialog_window wlist
+  return dialog_window
 
-createDialog :: [Window] -> IO Dialog
-createDialog = undefined
-
+createWindowList :: [Window] -> IO Gtk.TreeView
+createWindowList wins = Gtk.treeViewNewWithModel =<< Gtk.listStoreNew wins
+  
